f74a0069928cbd8439e6cad8a1a4fbeab42559ad
Felipe <fgiro@uliege.be>
2021-08-16 18:40:34 +0200

    test + new organization

---
+++ reliabpy/models/inference.py
@@ -0,0 +1,192 @@
+import numpy as np
+from scipy.stats import norm
+from reliabpy.models.observation import Probability_of_Detection as PoD
+
+# TODO: documentation
+
+class _Base(object):
+    def __init__(self, store_results=True):
+        self.store_results = store_results
+        self.t, self.obs, self.action = 0, None, None
+
+        self.force_detection =  False
+        self.force_notdetection = False
+
+    def _store_results(self):
+        if self.store_results:
+            self.results.append([
+                self.t, 
+                self.get_prob_fail(),
+                self.obs,
+                self.action
+                ])
+
+
+    def get_pf(self):
+        
+        if not self.store_results:
+            raise Warning("No stored probability of failure: store_results is", self.store_results)
+        
+        results = np.array(self.results)
+        time, pfs = results[:,0], results[:,1]
+
+        return time, pfs
+    
+    def get_results(self):
+
+        if not self.store_results:
+            raise Warning("No stored probability of failure: store_results is", self.store_results)
+
+        results = np.array(self.results)
+
+        return {"year" : results[:,0], "pf" : results[:,1], "obs" : results[:,2], "action" : results[:,3]}
+
+        
+        
+
+class MonteCarloSimulation(_Base):
+    def initialize(self, a_0, function, a_crit):
+        self.a_0 = a_0 
+        self.a = a_0.copy()
+        self.f = function
+        self.a_crit = a_crit
+
+        self.num_samples = len(self.a_0)
+
+        self.t = 0
+        self.PoD = np.ones_like(a_0)
+
+        if self.store_results:
+            self.results = [[
+                self.t, 
+                self.get_prob_fail(),
+                None,
+                None]]
+
+    def predict(self):
+        self.a = self.f()
+        self.t += 1
+
+        if self.store_results: self._store_results()
+
+    def update(self, parameters):
+        self.obs = 'PoD'
+        parameters, invPoD_func = PoD.get_settings(parameters['quality'], inverse=True)
+
+        uniform_dist = np.random.uniform(size=self.num_samples)
+        a_detected = invPoD_func(uniform_dist, **parameters)
+        samples_detected = self.a > a_detected
+        self.crack_detected = bool(np.random.binomial(1, samples_detected.sum()/self.num_samples))
+        if any([self.force_detection, self.force_notdetection]):
+            if self.force_detection:
+                gH = samples_detected
+            if self.force_notdetection:
+                gH = self.a <= a_detected
+        else:
+            if self.crack_detected:
+                gH = samples_detected
+            else:
+                gH = self.a <= a_detected
+        self.PoD = self.PoD*gH
+
+        if self.store_results: self._store_results()
+
+    def get_prob_fail(self):
+        failed_samples = self.a > self.a_crit
+
+        total = self.PoD.sum()
+        failed_detected = np.sum(failed_samples*self.PoD)
+
+        return failed_detected/total
+
+# TODO: Transition matrix class
+# write a class to built the transiton matrix for DBN from a given function. 
+class TransitionMatrix:
+    def __init__(self):
+        pass
+
+    def initialize(self, discretization, function):
+        pass
+
+    def built_T(self):
+        pass
+
+class DynamicBayesianNetwork(_Base):
+    '''
+    Component level dynamic Bayesian network
+    ========================================
+    '''
+
+    def _reorder(self):
+        return self.s.reshape(list(self.n_states.values()))
+    
+    def _discretize(discretization, dist_params, function):
+        dist_params['a'] = discretization
+        return np.diff(function(**dist_params))/np.diff(discretization)
+    
+    def initialize(self, T, discretizations, s0):
+        self.T = T
+        self.s = s0
+        self.discretizations = discretizations
+
+        self.states_values = np.diff(discretizations['a']/2) + discretizations['a'][:-1]
+
+        self.n_states = {}
+        self.total_nstates = 1
+        for var_name in discretizations:
+            quant = int(len(discretizations[var_name]) - 1)
+            self.n_states[var_name] = quant
+            self.total_nstates *= quant
+        
+        if self.store_results:
+            self.results = [[
+                self.t, 
+                self.get_prob_fail(),
+                None,
+                None]]
+
+    def predict(self):
+        self.t += 1
+        self.s = np.dot(self.s, self.T)
+        
+        if self.store_results: self._store_results()
+    
+    def update(self, model, parameters):
+        self.obs = model
+        if model == 'normal':
+            parameters, function = parameters, norm.cdf
+            obs_pmf = self._discretize(self.discretizations['a'], parameters, function)
+        elif model == 'PoD':
+            parameters, function = PoD.get_settings(parameters['quality'])
+            obs_pmf = function(self.states_values, **parameters)
+
+        obs_state = np.tile(obs_pmf, int(self.total_nstates/len(obs_pmf)))
+        detected_pmf = self.s*obs_state
+
+        self.crack_detected = bool(np.random.binomial(1, detected_pmf.sum()))
+        if any([self.force_detection, self.force_notdetection]):
+            if self.force_detection:
+                self.s = detected_pmf
+            if self.force_notdetection:
+                self.s = self.s*(1 - obs_state)
+        else:
+            if self.crack_detected:
+                self.s = detected_pmf
+            else:
+                self.s = self.s*(1 - obs_state)
+
+        self.s /= self.s.sum()
+        
+        if self.store_results: self._store_results()
+        
+    def get_prob_fail(self):
+        s = self._reorder()
+        return s.sum(axis=0)[-1]
+
+
+class metrics:
+    def pf_rmse(model_1, model_2):
+        pf1 = model_1.get_pf()[1]
+        pf2 = model_2.get_pf()[1]
+
+        return np.sqrt(((pf1 - pf2)**2.0).mean())
---
+++ reliabpy/tests/test_inference.py
@@ -0,0 +1,58 @@
+import unittest
+import numpy as np
+import numpy.testing as np_test
+from reliabpy.models.inference import MonteCarloSimulation, DynamicBayesianNetwork, metrics
+from reliabpy.readwrite.ANAST import *
+from reliabpy.models.deterioration import *
+from scipy.special import gamma
+
+class TestALL(unittest.TestCase):
+    def setUp(self):
+        # TODO: test imports
+        # change the import files
+        lnC_mean, lnC_std = import_calibrated_values("C:\\Developments\\reliabpy\PhD\\transition_matrices\\cal_out_atm.mat")
+        DFF, lifetime, a0_mean, a_crit, description, h, sn_params, m, q_cov, n_samples, n = import_component_inputs("C:\\Developments\\reliabpy\PhD\\transition_matrices\\0atm_SNparams.mat")
+        q_mean = import_weilbull_mean("C:\\Developments\\reliabpy\\PhD\\transition_matrices\\q_out_atm.mat")
+        q = np.random.normal(q_mean, q_mean*q_cov, n_samples)
+        C = np.random.lognormal(lnC_mean, lnC_std, n_samples)
+        a_0 = np.random.exponential(a0_mean, n_samples)
+        S = q*gamma(1.0+1.0/h)
+
+        Y_g = GeometricFactor.lognormal(n_samples=n_samples)
+        det_model = Paris_Erdogan()
+        det_model.initialize(a_0, m, n, C, S, Y_g)
+        function = det_model.propagate
+
+        inspection_years = [9, 17]
+
+        # DBN
+        T_path = "PhD\\transition_matrices\\dr_out_atm.mat"
+        T, b0, discretizations = import_DBN_input_data(T_path)
+
+        self.dbn = DynamicBayesianNetwork()
+        self.dbn.force_detection, self.dbn.force_notdetection = False, True
+        self.dbn.initialize(T, discretizations, b0)
+        while self.dbn.t <= lifetime:
+            self.dbn.predict()
+
+            if self.dbn.t in inspection_years:
+                self.dbn.update(model='PoD', parameters={'quality': 'bad'})
+
+        # MCS
+        self.mcs = MonteCarloSimulation()
+        self.mcs.force_detection, self.mcs.force_notdetection = False, True
+        self.mcs.initialize(a_0, function, a_crit)
+
+        while self.mcs.t <= lifetime:
+            self.mcs.predict()
+            
+            if self.mcs.t in inspection_years:
+                self.mcs.update(parameters={'quality': 'bad'})
+
+    def test_rmse(self):
+        rmse_pf = metrics.pf_rmse(self.mcs, self.dbn)
+
+        self.assertLess(rmse_pf, 1e-3)
+
+if __name__ == '__main__':
+    unittest.main()
