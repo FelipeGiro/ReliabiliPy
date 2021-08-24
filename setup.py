41315a239b6e7f41377af9906760801cf00752be
Felipe <fgiro@uliege.be>
2021-08-24 11:08:12 +0200

    version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='reliabpy',
     packages=find_packages(),
-    version='0.0.0',
+    version='0.1.0',
     description='System level for structural reliability analysis for Python',
     author='Felipe Giro',
     license='MIT',
