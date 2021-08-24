d03527c7acfd865e9562b4e13c0fed735f6d645d
Felipe <fgiro@uliege.be>
2021-08-24 09:25:28 +0200

    add docstring to cost models

--- reliabpy/models/cost.py
+++ reliabpy/models/cost.py
@@ -6,11 +6,45 @@
 import numpy as np
 
 class InspectionMaintenance:
+    """
+    Inspection and Maintenance
+    ==========================
+    
+    Cost calculation for inspection and maintenance reliability analysis.
+
+    Parameters:
+    -----------
+    c_c : float
+        individual cost of campaign
+    c_i : float 
+        individual cost of inspection
+    c_r : float
+        individual cost of repair
+    c_f : float
+        individual cost of failure
+    r : float
+        discount rate
+    """
     def __init__(self, c_c=5.0, c_i=1.0, c_r=10.0, c_f=10000, r=0.02):
         self.c_c, self.c_i, self.c_r, self.c_c, self.c_f, self.r =  c_c, c_i, c_r, c_c, c_f, r
     
     def compute_cost_breakdown(self, system_model):
+        """
+        Compute cost breakdown
+        ======================
         
+        From simulated model, compute all lifetime costs:
+        - campaign   : C_C
+        - inspection : C_I
+        - repair     : C_R
+        - failure    : C_F
+
+        Results are stored in the system atributes in dictionaries:
+        - system_obs
+        - system_action
+        - yearly_costs_breakdown
+        - cost_breakdown
+        """
         system_obs, system_action = dict(), dict()
         t, pf = np.vstack(system_model.system_pf).T
         unique_mask = np.diff(t) == 1
