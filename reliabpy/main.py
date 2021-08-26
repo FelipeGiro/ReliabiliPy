e4409f7544684ee87bf65f9c48bc818c8d0c34da
Felipe <fgiro@uliege.be>
2021-08-26 13:57:57 +0200

    print elapsed time

--- reliabpy/main.py
+++ reliabpy/main.py
@@ -8,15 +8,18 @@ def run():
 if __name__ == '__main__':
     import cProfile, pstats
     import subprocess
+    import time
     model = Simple()
     model.mount_model()
     profiler = cProfile.Profile()
+    start = time.time()
     profiler.enable()
     model.run_one_episode()
     profiler.disable()
+    print("Elapsed time for one episode", time.time() - start, 's')
     stats = pstats.Stats(profiler).sort_stats('tottime')
     stats.sort_stats('tottime')
     stats.reverse_order()
-    stats.print_stats()
-    stats.dump_stats('profile.dat')
-    subprocess.call(r"snakeviz profile.dat")
+    # stats.print_stats()
+    # stats.dump_stats('profile.dat')
+    # subprocess.call(r"snakeviz profile.dat")
