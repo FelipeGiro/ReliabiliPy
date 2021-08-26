747fc727773ce6065d0ff93e1905acebee2038f9
Felipe <fgiro@uliege.be>
2021-08-26 13:55:40 +0200

    to profile only the calculation

--- reliabpy/main.py
+++ reliabpy/main.py
@@ -8,13 +8,15 @@ def run():
 if __name__ == '__main__':
     import cProfile, pstats
     import subprocess
+    model = Simple()
+    model.mount_model()
     profiler = cProfile.Profile()
     profiler.enable()
-    run()
+    model.run_one_episode()
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('tottime')
     stats.sort_stats('tottime')
     stats.reverse_order()
-    # stats.print_stats()
-    # stats.dump_stats('profile.dat')
-    # subprocess.call(r"snakeviz profile.dat")
+    stats.print_stats()
+    stats.dump_stats('profile.dat')
+    subprocess.call(r"snakeviz profile.dat")
